e7faf69ca47280ff1036fbe97ae019b13bb16a5c
rashik-raj <rashik123.rs@gmail.com>
2021-08-28 21:45:25 +0545

    Add migrations in procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn vendor_machine.wsgi
+release: python manage.py makemigrations
+release: python manage.py migrate
--- vendor_machine/settings.py
+++ vendor_machine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qp+aop#mho51%yta2iurr5jw6)3r6^ai81^&v5j9&ut59*+3(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
