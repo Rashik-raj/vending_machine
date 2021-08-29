d8dea4fde76981b6a6ee9729727ef09b53f9abad
rashik-raj <rashik123.rs@gmail.com>
2021-08-29 09:54:12 +0545

    added static files serve

--- vendor_machine/urls.py
+++ vendor_machine/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 
 admin.site.site_header = "Admin Vender"
 admin.site.site_title = "Vender Admin Portal"
@@ -25,7 +25,6 @@ admin.site.index_title = "Welcome to Vender Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vendor.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
