1a256ca0af85af32ef3a69d92cdbee42b3d292ba
rashik-raj <rashik123.rs@gmail.com>
2021-08-29 10:04:45 +0545

    added assets

--- vendor_machine/settings.py
+++ vendor_machine/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
